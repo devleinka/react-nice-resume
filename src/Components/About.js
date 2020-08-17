f634d1a573e1ee89a1aedd69e6bd80445dc80afa
Lea Mikulcová <42700247+devleinka@users.noreply.github.com>
2020-08-17 15:17:52 +0200

    minor changes and fixes

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,16 +4,8 @@ class About extends Component {
   render() {
 
     if(this.props.data){
-      var name = this.props.data.name;
       var profilepic= "images/"+this.props.data.image;
       var bio = this.props.data.bio;
-      var linkedin = this.props.data.linkedin;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
     }
 
     return (
@@ -26,13 +18,7 @@ class About extends Component {
             <h2>About Me</h2>
 
             <p>{bio}</p>
-            <div className="row">
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
-            </div>
+           
          </div>
       </div>
 
