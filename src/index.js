c88d1dd30b6b87b28d1bae437ce9932cf829d82f
Lea Mikulcová <42700247+devleinka@users.noreply.github.com>
2020-08-20 14:01:36 +0200

    updated GA, added GTM

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+    gtmId: 'GTM-NNP568M'
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
